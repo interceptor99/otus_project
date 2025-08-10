28456817326ff199bf0d66344201f9cc955b7b66
sorokin_ia <ilya.sorokin@c3dlabs.com>
2025-08-10 13:16:23 +0300

    Добавление производных классов по расчету максимума, среднего арифметического и среднеквадратического отклонения

--- Source/statistics.cpp
+++ Source/statistics.cpp
@@ -12,7 +12,7 @@ public:
 
 class Min : public IStatistics {
 public:
-	Min() : m_min{std::numeric_limits<double>::min()} {
+	Min() : m_min{std::numeric_limits<double>::max()} {
 	}
 
 	void update(double next) override {
@@ -31,6 +31,75 @@ public:
 
 private:
 	double m_min;
+};
+
+class Max : public IStatistics {
+public:
+	Max() : m_max{ std::numeric_limits<double>::min() } {
+	}
+
+	void update( double next ) override {
+		if ( next > m_max ) {
+			m_max = next;
+		}
+	}
+
+	double eval() const override {
+		return m_max;
+	}
+
+	const char * name() const override {
+		return "max";
+	}
+
+private:
+	double m_max;
+};
+
+class Mean : public IStatistics {
+public:
+	Mean() : sum{ 0. }, count{ 0 } {
+	}
+
+	void update( double next ) override {
+		++count;
+		sum += next;
+	}
+
+	double eval() const override {
+		return ( count > 0 ) ? sum / count : 0.;
+	}
+
+	const char * name() const override {
+		return "mean";
+	}
+
+private:
+	double sum;
+	size_t count;
+};
+
+class Std : public IStatistics {
+public:
+	Std() : sumSqr{ 0. }, count{ 0 } {
+	}
+
+	void update( double next ) override {
+		++count;
+		sumSqr += next * next;
+	}
+
+	double eval() const override {
+		return ( count > 0 ) ? ::sqrt( sumSqr / count ) : 0.;
+	}
+
+	const char * name() const override {
+		return "std";
+	}
+
+private:
+	double sumSqr;
+	size_t count;
 };
 
 int main() {
